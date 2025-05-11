2d2ef061ab807ca193051a5669f5d9f1f847e218
CoderAayushDodia <coderaayush369@gmail.com>
2025-05-11 18:30:57 +0530

    Menu bar

--- biodata/js/app.js
+++ biodata/js/app.js
@@ -1,29 +1,21 @@
-const bar = document.getElementById("bar");
-const nav = document.getElementById("about");
-const close = document.getElementById("close");
+const menuToggle = document.getElementById("menu-toggle");
+const sideMenu = document.getElementById("sideMenu");
+const closeMenu = document.getElementById("close-menu");
 
-if(bar){
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-        
-        console.log("clicked");
-    })
-}
-
-if(close){
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-        console.log("press")
-    })
-}
+menuToggle.addEventListener("click", () => {
+  sideMenu.classList.add("active");
+});
+closeMenu.addEventListener("click", () => {
+  sideMenu.classList.remove("active");
+});
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-      }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
   });
+});
 
-  const elements = document.querySelectorAll('.animate-on-scroll');
-  elements.forEach((el) => observer.observe(el));
+const elements = document.querySelectorAll(".animate-on-scroll");
+elements.forEach((el) => observer.observe(el));
