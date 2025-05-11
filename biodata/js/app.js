702c447bfe0c65f3323cba15ba328a8388ae61b0
CoderAayushDodia <coderaayush369@gmail.com>
2025-05-11 17:15:08 +0530

    Animation

--- biodata/js/app.js
+++ biodata/js/app.js
@@ -16,3 +16,14 @@ if(close){
         console.log("press")
     })
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  });
+
+  const elements = document.querySelectorAll('.animate-on-scroll');
+  elements.forEach((el) => observer.observe(el));
